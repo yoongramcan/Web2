1a0b6c1e2451d023ed5cbd5a7248d59b7a8f3703
Pink Nye <kpnye@massart.edu>
2015-11-15 11:08:08 -0500

    Mick Fanning

---
+++ hw_05_logoish/pattern.js
@@ -0,0 +1,26 @@
+//Draw a Triangle
+
+speed(150)
+lineWidth(1);
+lineColor("red");
+
+var x=60;
+rotate(60);
+forward(x); //forward(x)
+rotate(60);
+forward(x);
+rotate(60);
+forward(x);
+rotate(60);
+forward(x);
+rotate(60);
+forward(x);
+rotate(60);
+forward(x);
+
+
+
+// below is Loop
+for(var y=0; y<4; y++) {
+  MadeUpName(y*60+60);
+}
--- scrolling_story/main.js
+++ scrolling_story/main.js
@@ -1,15 +1,65 @@
 $(".scroller").scrollFlight();
 
-$(".title").on("arrived",function() {
-  $(this).addClass("arrived");
-});
-$(".quote").on("arrived",function() {
-  $(this).addClass("arrived");
-});
-$(".text_block").on("arrived",function() {
-  $(this).addClass("text-block-arrived");
-});
-
 var video = $("#underwater")[0];
 video.currentTime=10
 video.play()
+
+	$(".title").on("arrived",function() {
+	  $(this).addClass("arrived");
+	});
+
+	$(".quote").on("arrived",function() {
+	  $(this).addClass("arrived");
+	});
+
+
+
+//Text Block 1
+   $(".text-block-1").on("arriving", function(e) {
+    $(this).addClass("text-block-arriving");
+	});
+   
+	$(".text-block-1").on("departing", function(e) {
+    $(this).addClass("text-block-departing");
+	});
+
+	$(".text-block-1").on("rearriving", function(e) {
+    $(this).addClass("text-block-rearriving");
+	});
+
+//Text Block 2
+   $(".text-block-2").on("arriving", function(e) {
+    $(this).addClass("text-block-arriving");
+	});
+   
+//Text Block 3
+   $(".text-block-3").on("arriving", function(e) {
+    $(this).addClass("text-block-arriving");
+	});
+   
+	$(".text-block-3").on("departing", function(e) {
+    $(this).addClass("text-block-departing");
+	});
+
+	$(".text-block-3").on("rearriving", function(e) {
+    $(this).addClass("text-block-rearriving");
+	});
+
+
+
+
+//Map
+   $(".map").on("arriving", function(e) {
+    $(this).addClass("map-enlarge");
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
