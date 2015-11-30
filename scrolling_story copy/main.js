46153bdc6c584928a886d1e1aac16c4cea17be60
Pink Nye <kpnye@massart.edu>
2015-11-30 09:08:48 -0500

    poster

--- scrolling_story copy/main.js
+++ scrolling_story copy/main.js
@@ -20,7 +20,7 @@ video.play()
    $(".text-block-1").on("arriving", function(e) {
     $(this).addClass("text-block-arriving");
 	});
-   
+
 	$(".text-block-1").on("departing", function(e) {
     $(this).addClass("text-block-departing");
 	});
@@ -33,12 +33,12 @@ video.play()
    $(".text-block-2").on("arriving", function(e) {
     $(this).addClass("text-block-arriving");
 	});
-   
+
 //Text Block 3
    $(".text-block-3").on("arriving", function(e) {
     $(this).addClass("text-block-arriving");
 	});
-   
+
 	$(".text-block-3").on("departing", function(e) {
     $(this).addClass("text-block-departing");
 	});
@@ -51,29 +51,18 @@ video.play()
    $(".text-block-4").on("arriving", function(e) {
     $(this).addClass("text-block-arriving");
 	});
-	
+
 //Text Block 5
    $(".text-block-5").on("arriving", function(e) {
     $(this).addClass("text-block-arriving");
 	});
-	
-	
+
+
 
 
 //Map
-	$("#page5").scrollFlight();
-	$("#page5").on("update",function(e, size) {
+/*	$("#page6").scrollFlight();
+	$("#page6").on("update",function(e, size) {
 	   console.log(size);
 	  $(".map").css({transform: "scale(" + (1 + size / 1) + ")" });
-	});
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
+	});  */
